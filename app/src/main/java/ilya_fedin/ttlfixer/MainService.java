71f874ed5fc7eecf6f30e2f4a31366899e0c478a
Ilya Fedin <fedin-ilja2010@ya.ru>
2017-07-21 22:39:44 +0400

    Повысил версию, реализовал возможность перефиксации без перезагрузки, улучшил определение ошибок

--- app/src/main/java/ilya_fedin/ttlfixer/MainService.java
+++ app/src/main/java/ilya_fedin/ttlfixer/MainService.java
@@ -16,16 +16,16 @@ import java.util.Scanner;
 import static java.lang.Runtime.getRuntime;
 
 public class MainService extends Service {
-    Context context;
-    Resources res;
+    private Context context;
+    private Resources res;
     private static final String DEFAULT_TTL = "64";
-    String ttl = DEFAULT_TTL;
+    private String ttl = DEFAULT_TTL;
 
-    void successNotification() {
+    private void successNotification() {
         NewNotification.notify(context, "result_notification", res.getString(R.string.ttl_success) + ": " + ttl);
     }
 
-    void errorNotification(String stdout, String stderr) {
+    private void errorNotification(String stdout, String stderr) {
         String notificationText = res.getString(R.string.ttl_error);
         if(!stdout.isEmpty() || !stderr.isEmpty()) notificationText += ":";
         if(!stdout.isEmpty()) notificationText += "\n" + stdout;
@@ -33,10 +33,128 @@ public class MainService extends Service {
         NewNotification.notify(context, "result_notification", notificationText);
     }
 
+    private void createChain() {
+        try {
+            Process ttl_fix = getRuntime().exec(new String[] {"su", "-c", "iptables -t mangle -N TTLFixer"});
+            int ttl_fix_code = ttl_fix.waitFor();
+
+            Scanner outputScanner = new Scanner(ttl_fix.getInputStream()).useDelimiter("\\A");
+            Scanner errorScanner = new Scanner(ttl_fix.getErrorStream()).useDelimiter("\\A");
+            String output = outputScanner.hasNext() ? outputScanner.next() : "";
+            String error = errorScanner.hasNext() ? errorScanner.next() : "";
+            if(!(output.isEmpty() && error.isEmpty() && ttl_fix_code == 0)) {
+                if(Integer.parseInt(ttl) != 0) errorNotification(output, error);
+                System.exit(0);
+            }
+        } catch (IOException | InterruptedException e) {
+            if(Integer.parseInt(ttl) != 0) errorNotification(e.toString(), "");
+            System.exit(0);
+        }
+
+        try {
+            Process ttl_fix = getRuntime().exec(new String[] {"su", "-c", "iptables -t mangle -A POSTROUTING -j TTLFixer"});
+            int ttl_fix_code = ttl_fix.waitFor();
+
+            Scanner outputScanner = new Scanner(ttl_fix.getInputStream()).useDelimiter("\\A");
+            Scanner errorScanner = new Scanner(ttl_fix.getErrorStream()).useDelimiter("\\A");
+            String output = outputScanner.hasNext() ? outputScanner.next() : "";
+            String error = errorScanner.hasNext() ? errorScanner.next() : "";
+            if(!(output.isEmpty() && error.isEmpty() && ttl_fix_code == 0)) {
+                if(Integer.parseInt(ttl) != 0) errorNotification(output, error);
+                System.exit(0);
+            }
+        } catch (IOException | InterruptedException e) {
+            if(Integer.parseInt(ttl) != 0) errorNotification(e.toString(), "");
+            System.exit(0);
+        }
+    }
+
+    private void flushChain() {
+        try {
+            Process ttl_fix = getRuntime().exec(new String[] {"su", "-c", "iptables -t mangle -F TTLFixer"});
+            int ttl_fix_code = ttl_fix.waitFor();
+
+            Scanner outputScanner = new Scanner(ttl_fix.getInputStream()).useDelimiter("\\A");
+            Scanner errorScanner = new Scanner(ttl_fix.getErrorStream()).useDelimiter("\\A");
+            String output = outputScanner.hasNext() ? outputScanner.next() : "";
+            String error = errorScanner.hasNext() ? errorScanner.next() : "";
+            if(!(output.isEmpty() && error.isEmpty() && ttl_fix_code == 0)) {
+                if(Integer.parseInt(ttl) != 0) errorNotification(output, error);
+                System.exit(0);
+            }
+        } catch (IOException | InterruptedException e) {
+            if(Integer.parseInt(ttl) != 0) errorNotification(e.toString(), "");
+            System.exit(0);
+        }
+    }
+
+    private void removeChain() {
+        try {
+            Process ttl_fix = getRuntime().exec(new String[] {"su", "-c", "iptables -t mangle -D POSTROUTING -j TTLFixer"});
+            int ttl_fix_code = ttl_fix.waitFor();
+
+            Scanner outputScanner = new Scanner(ttl_fix.getInputStream()).useDelimiter("\\A");
+            Scanner errorScanner = new Scanner(ttl_fix.getErrorStream()).useDelimiter("\\A");
+            String output = outputScanner.hasNext() ? outputScanner.next() : "";
+            String error = errorScanner.hasNext() ? errorScanner.next() : "";
+            if(!(output.isEmpty() && error.isEmpty() && ttl_fix_code == 0)) {
+                if(Integer.parseInt(ttl) != 0) errorNotification(output, error);
+                System.exit(0);
+            }
+        } catch (IOException | InterruptedException e) {
+            if(Integer.parseInt(ttl) != 0) errorNotification(e.toString(), "");
+            System.exit(0);
+        }
+
+        flushChain();
+
+        try {
+            Process ttl_fix = getRuntime().exec(new String[] {"su", "-c", "iptables -t mangle -X TTLFixer"});
+            int ttl_fix_code = ttl_fix.waitFor();
+
+            Scanner outputScanner = new Scanner(ttl_fix.getInputStream()).useDelimiter("\\A");
+            Scanner errorScanner = new Scanner(ttl_fix.getErrorStream()).useDelimiter("\\A");
+            String output = outputScanner.hasNext() ? outputScanner.next() : "";
+            String error = errorScanner.hasNext() ? errorScanner.next() : "";
+            if(!(output.isEmpty() && error.isEmpty() && ttl_fix_code == 0)) {
+                if(Integer.parseInt(ttl) != 0) errorNotification(output, error);
+                System.exit(0);
+            }
+        } catch (IOException | InterruptedException e) {
+            if(Integer.parseInt(ttl) != 0) errorNotification(e.toString(), "");
+            System.exit(0);
+        }
+    }
+
+    private boolean isChainExists() {
+        boolean exist = false;
+        try {
+            Process ttl_fix = getRuntime().exec(new String[] {"su", "-c", "iptables -t mangle -S"});
+            int ttl_fix_code = ttl_fix.waitFor();
+
+            Scanner outputScanner = new Scanner(ttl_fix.getInputStream()).useDelimiter("\\A");
+            Scanner errorScanner = new Scanner(ttl_fix.getErrorStream()).useDelimiter("\\A");
+            String output = outputScanner.hasNext() ? outputScanner.next() : "";
+            String error = errorScanner.hasNext() ? errorScanner.next() : "";
+
+            if(!(error.isEmpty() && ttl_fix_code == 0)) {
+                if(Integer.parseInt(ttl) != 0) errorNotification(output, error);
+                System.exit(0);
+            }
+
+            exist = output.contains("-N TTLFixer");
+        } catch (IOException | InterruptedException e) {
+            if(Integer.parseInt(ttl) != 0) errorNotification(e.toString(), "");
+            System.exit(0);
+        }
+        return exist;
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         context = getApplicationContext();
         res = context.getResources();
+
         File sdcard = Environment.getExternalStorageDirectory();
         File ttl_file = new File(sdcard, ".fix_ttl");
         try {
@@ -46,29 +164,46 @@ public class MainService extends Service {
         } catch (IOException e) {
             ttl = DEFAULT_TTL;
         }
-        try {
-            if(ttl == null || ttl.trim().isEmpty()) ttl = DEFAULT_TTL;
-            ttl = ttl.trim();
-            if(!ttl.matches("\\d+") || Integer.parseInt(ttl) < 0 || Integer.parseInt(ttl) > 255) {
-                errorNotification(res.getString(R.string.ttl_incorrect_value), "");
+
+        if(ttl == null || ttl.trim().isEmpty()) ttl = DEFAULT_TTL;
+        ttl = ttl.trim();
+        if(!ttl.matches("\\d+") || Integer.parseInt(ttl) < 0 || Integer.parseInt(ttl) > 255) {
+            NewNotification.notify(context, "result_notification", res.getString(R.string.ttl_incorrect_value));
+            System.exit(0);
+        }
+
+        if(!RootUtil.isDeviceRooted()) {
+            if(Integer.parseInt(ttl) != 0) NewNotification.notify(context, "result_notification", res.getString(R.string.root_error));
+            System.exit(0);
+        }
+
+        if (isChainExists()) {
+            if(Integer.parseInt(ttl) == 0) {
+                removeChain();
                 System.exit(0);
             }
+            else flushChain();
+        } else {
             if(Integer.parseInt(ttl) == 0) System.exit(0);
-            Process ttl_fix = getRuntime().exec(new String[] {"su", "-c", "iptables -t mangle -A POSTROUTING -j TTL --ttl-set " + ttl});
+            else createChain();
+        }
+
+        try {
+            Process ttl_fix = getRuntime().exec(new String[] {"su", "-c", "iptables -t mangle -A TTLFixer -j TTL --ttl-set " + ttl});
             int ttl_fix_code = ttl_fix.waitFor();
+
             Scanner outputScanner = new Scanner(ttl_fix.getInputStream()).useDelimiter("\\A");
             Scanner errorScanner = new Scanner(ttl_fix.getErrorStream()).useDelimiter("\\A");
             String output = outputScanner.hasNext() ? outputScanner.next() : "";
             String error = errorScanner.hasNext() ? errorScanner.next() : "";
-            if(output.isEmpty() && error.isEmpty()) {
-                if (ttl_fix_code == 0) successNotification();
-                else errorNotification(output, error);
-            } else {
+            if(output.isEmpty() && error.isEmpty() && ttl_fix_code == 0)
+                successNotification();
+            else
                 errorNotification(output, error);
-            }
         } catch (IOException | InterruptedException e) {
             errorNotification(e.toString(), "");
         }
+
         Intent stopServiceIntent = new Intent(context, this.getClass());
         context.stopService(stopServiceIntent);
         System.exit(0);
